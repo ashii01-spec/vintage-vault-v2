bdf069f5b3999d7a10cea5340aa981fe28ce1666
ashii01-spec <ashmidurandararathnayake@gmail.com>
2026-01-26 22:33:47 +0530

    Theme Tweaks on Login and Signup Pages

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,12 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-vintage-900 relative overflow-hidden">
+    <div class="absolute top-0 left-0 w-full h-full opacity-10 pointer-events-none" 
+             style="background-image: url('https://www.transparenttextures.com/patterns/aged-paper.png');">
+    </div>
+    <div class="relative z-10">
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-vintage-50 border border-vintage-100 shadow-xl overflow-hidden sm:rounded-lg relative z-10">
         {{ $slot }}
     </div>
 </div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         @livewireStyles
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="font-serif text-gray-900 antialiased">
             {{ $slot }}
         </div>
 
